d1a8e8413275f1a8712174d0227c0e9bced18f21
kittyMons <puteriaqilah2609@gmail.com>
2024-07-07 00:41:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,97 +1,82 @@
 import streamlit as st
-import base64
+import pathlib as Path
 import google.generativeai as genai
-from openai import OpenAI
 
-client = OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
+genai.configure(api_key=st.secrets['GOOGLE_API_KEY'])
 
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
-st.set_page_config(
-    page_title="Outfit Suggestion App",
-    page_icon="👗",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #C9A9A6;
+    # Set up the model
+generation_config = {  "temperature": 0.4,  "top_p": 1,  "top_k": 32,
+    "max_output_tokens": 4096,
     }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
-st.title("👗 What's your outfit today?")
+safety_settings = [{"category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
+    {"category": "HARM_CATEGORY_HATE_SPEECH",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",  "threshold": "BLOCK_MEDIUM_AND_ABOVE"  }, 
+    {   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",   "threshold": "BLOCK_MEDIUM_AND_ABOVE" },
+    ]
 
-def ai_suggestion(occasion, prompt):
-  """
-  This function uses OpenAI's gpt-4o model to suggest an outfit based on occasion.
+system_prompt=""" 
+     As a highly skilled medical practitioner specializing in image analysis, you are tasked with examining medical images for a renowned hospital.Your expertise is curcial in identifyning any anomalies, diseases, or health issues that may be present in the images.
 
-  Args:
-      occasion: The occasion for the outfit suggestion (e.g., restaurant date).
-      prompt: A string prompting the model for outfit suggestions.
+     Your Responsibilities include:
 
-  Returns:
-      A string containing the suggested outfit.
-  """  
-  fashion_response = client.chat.completions.create(
-      model="gpt-4o",
-      messages=[{
-          "role": "system",
-          "content": prompt
+     1.Detailed Analysis: Thoroughly analyze each image, focusing on identifying any abnormal findings.
+     2.Findings Report: Document all observed anomalies or signs of disease.Clearly articulate these findings in a structured format.
+     3.Recommendation and Next Steps: Based on your analysis, suggest potential next steps, including further tests or treatments as applicable.
+     4.Treatment Suggestions: If appropriate, recommend possible treatment options or interventions.
 
-      },
-          {
-          "role": "user",
-          "content": occasion
-      }],
+     Important Notes:
 
-      max_tokens=50
+     1.Scope of Response: Only respond if the image pertains to human health issues.
+     2.Clarity of Image: In cases where image quality impedes clear analysis, note that certain aspects are 'Unable to be determined based on the provided image.'
+     3.Disclaimer:Accompany your analysis with the disclaimer :" Consult with a Doctor before making any decisions."
+     4.Your insights are invaluable in guiding clinical decisions. Please proceed with the analysis adhering to the structured approach outlined above.
 
-  )
-  
-  fashion = fashion_response.choices[0].message.content
-  return fashion
+     Please provide me an output response with these 4 headings and also provide output response according to the Disclaimer
+     """
 
-def encode_image(file):
-  """
-  This function encodes an uploaded image file to base64 format.
+model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config, safety_settings=safety_settings)
 
-  Args:
-      file: A Streamlit file uploader object containing the image data.
 
-  Returns:
-      A string containing the base64 encoded image data.
-  """
-  bytes_data = file.read()
-  return base64.b64encode(bytes_data).decode('utf-8')
 
-uploaded_files = st.file_uploader("Upload pictures of clothes or accessories", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
-# Display uploaded images in a horizontal row
-uploaded_images = []
-if uploaded_files:
-  columns = st.columns(len(uploaded_files))
-  for column, uploaded_file in zip(columns, uploaded_files):
-    bytes_data = uploaded_file.read()
-    column.image(bytes_data, use_column_width=True)
-    uploaded_images.append(f"data:image/png;base64,{encode_image(uploaded_file)}")
 
-occasion = st.text_input("Enter the occasion for which you need an outfit suggestion (e.g., restaurant date)")
 
-if st.button("Get Suggestion"):
-  if occasion:
-    if uploaded_images:
-      # Use Gemini API for suggestion with uploaded images
-      prompt = f"You are a fashion assistant. Based on the following items and the occasion ({occasion}), suggest an outfit and provide feedback on the uploaded outfits ({uploaded_images}):"
-      suggestion = ai_suggestion(occasion, prompt)
-    else:
-      # Use OpenAI for suggestion without uploaded images
-      suggestion = ai_suggestion(occasion, f"Suggest an outfit for {occasion}.")
-    st.markdown(suggestion)  # Display suggestion formatted with markdown
-  else:
-    st.write("Please enter an occasion.")
+    #page configuration
+st.set_page_config(page_title="VitalImage Analytics",page_icon=":robot")
+
+    #title
+st.title("Vital Image Analytics 👩‍⚕️🩺")
+
+st.subheader("An application designed to assist in the identification of medical images")
+
+uploaded_file=st.file_uploader("Upload the medical image for analysis", type=["png","jpg","jpeg"])
+if uploaded_file:
+    st.image(uploaded_file, caption="Uploaded Image")
+    submit_button=st.button("Generate the Analysis")
+
+    if submit_button:
+        image_data=uploaded_file.getvalue()
+
+        image_parts=[
+            {
+                "mime_type":"image/jpeg",
+                "data":image_data
+            },
+        ]
+
+        prompt_parts=[
+            image_parts[0],
+            system_prompt,
+        ]
+
+        response=model.generate_content(prompt_parts)
+        if response:
+            st.title("Here is the analysis based on your image: ")
+            st.write(response.text)
+
+
+
+
